9cc3eb00b0d4843796f057ed78e6bee288820696
StanislavBatov <125702005+StanislavBatov@users.noreply.github.com>
2023-05-08 18:10:52 +0300

    4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerRef = document.querySelector('#vimeo-player');
+const player = new Player(playerRef);
+
+if (localStorage.getItem("videoplayer-current-time")) {  
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+}
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}; 
+
+player.on('timeupdate', throttle(onPlay, 1000));
