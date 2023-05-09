d07482f22ffedabce984b05e0fbd1df6092ef37c
StanislavBatov <125702005+StanislavBatov@users.noreply.github.com>
2023-05-10 00:34:29 +0300

    5

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+import storeService from './json.js';
 
+const formRef = document.querySelector('.feedback-form');
+
+if (localStorage.getItem("feedback-form-state")) {
+    const savedInputs = storeService.load("feedback-form-state")
+    for (const input in savedInputs) {
+        if (savedInputs[input]) {
+            formRef[input].value = savedInputs[input];
+        }
+    }
+}
+
+
+formRef.addEventListener('input', throttle(onInputForm, 500))
+formRef.addEventListener('submit', onSubmitForm)
+
+
+let inputHolder = {};
+
+function onInputForm(e) {
+    inputHolder[e.target.name] = e.target.value;
+    storeService.save("feedback-form-state", inputHolder);
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  const emailValue = formRef.email.value;
+    console.log(inputHolder);
+    localStorage.removeItem("feedback-form-state");
+  formRef.reset();
+}
